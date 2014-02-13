f447d700fbe0e72920fcf9e44e0c93749445b75e
peyanski <kirile@gmail.com>
2014-02-13 17:04:22 +0200

    until listing 5.1.3

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -10,10 +10,15 @@ describe "let" do
       expect(page).to have_content('Sample App')
     end
 	
-	it "should have the title 'Home'" do
+	it "should have the base title" do
       visit '/static_pages/home'
-      expect(page).to have_title("#{base_title} | Home")
+      expect(page).to have_title("#{base_title}")
     end
+	
+	it "should have dynamic base title" do
+	  visit '/static_pages/home'
+	  expect(page).not_to have_title('| Home')
+	end
   end
   
   describe "Help page" do
